78613649b22c7606983b17cbc53077ade53eb717
Sanjay3556 <110145049+Sanjay3556@users.noreply.github.com>
2022-08-01 14:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def main():
       result=predict_random(UserID, Gender,Age,EstimatedSalary)
       st.success('Random forest Model  has predicted {}'.format(result))  
     if st.button("About"):
-      st.header("Developed by Deepak Moud")
-      st.subheader("Head , Department of Computer Engineering")
+      st.header("Developed by Sanjay Singh")
+      st.subheader("Student, Department of Computer Engineering")
     html_temp = """
     <div class="" style="background-color:orange;" >
     <div class="clearfix">           
